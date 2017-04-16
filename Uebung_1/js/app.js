2d4cb5d50349188c100abc6dc611022e2b318fa9
Paul Grundmann <paul.grundmann@snafu.de>
2017-04-16 19:58:40 +0200

    like/dislike-button working with states

--- Uebung_1/js/app.js
+++ Uebung_1/js/app.js
@@ -1,41 +1,38 @@
 window.onload = function () {
 
-    var classname = document.getElementsByClassName("img");
-
-
-    for (var i = 0; i < classname.length; i++) {
-        classname[i].addEventListener('mouseover', function(event) {
+    var imgClassList = document.getElementsByClassName("img");
+    for (var i = 0; i < imgClassList.length; i++) {
+        imgClassList[i].addEventListener('mouseenter', function (event) {
             var target = event.target;
-            var btn1 = target.nextElementSibling.firstElementChild;
-            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            var btn1 = target.firstElementChild.nextElementSibling.firstElementChild;
+            var btn2 = target.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
             btn1.style.visibility = "visible";
             btn1.style.opacity = 1;
             btn2.style.visibility = "visible";
             btn2.style.opacity = 1;
-
         });
-        classname[i].addEventListener('mouseout', function(event) {
+        imgClassList[i].addEventListener('mouseleave', function (event) {
             var target = event.target;
-            var btn1 = target.nextElementSibling.firstElementChild;
-            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            var btn1 = target.firstElementChild.nextElementSibling.firstElementChild;
+            var btn2 = target.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
             btn1.style.visibility = "hidden";
             btn1.style.opacity = 0;
-            btn2.style.opacity = 0;
             btn2.style.visibility = "hidden";
+            btn2.style.opacity = 0;
         });
-        classname[i].addEventListener('touchstart', function(event) {
+        imgClassList[i].addEventListener('touchstart', function (event) {
             var target = event.target;
-            var btn1 = target.nextElementSibling.firstElementChild;
-            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            var btn1 = target.firstElementChild.nextElementSibling.firstElementChild;
+            var btn2 = target.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
             btn1.style.visibility = "visible";
-            btn2.style.visibility = "visible";
             btn1.style.opacity = 1;
+            btn2.style.visibility = "visible";
             btn2.style.opacity = 1;
         });
-        classname[i].addEventListener('touchend', function(event) {
+        imgClassList[i].addEventListener('touchend', function (event) {
             var target = event.target;
-            var btn1 = target.nextElementSibling.firstElementChild;
-            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            var btn1 = target.firstElementChild.nextElementSibling.firstElementChild;
+            var btn2 = target.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
             btn1.style.visibility = "hidden";
             btn1.style.opacity = 0;
             btn2.style.visibility = "hidden";
@@ -44,7 +41,20 @@ window.onload = function () {
         });
     }
 
+    var likeButtonClassList = document.getElementsByClassName("likeButton");
+    for (var i = 0; i < likeButtonClassList.length; i++) {
+        likeButtonClassList[i].addEventListener('click', function (event) {
+            var btn = event.target;
+            if(btn.style.transform == "" || btn.style.transform == "rotate(-180deg)") {
+                btn.style.transform = "rotate(0deg)";
+            }
+            else
+            {
+                btn.style.transform = "rotate(-180deg)";
+            }
 
+        });
+    }
 
 
 };

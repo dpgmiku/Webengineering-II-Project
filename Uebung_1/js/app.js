b2547ad0083b635a7e73907d491217d6e8a67d9f
Paul Grundmann <paul.grundmann@snafu.de>
2017-04-16 19:37:46 +0200

    button overlay now working; transition effect on hover

--- Uebung_1/js/app.js
+++ Uebung_1/js/app.js
@@ -1,49 +1,50 @@
 window.onload = function () {
 
-    var images = document.getElementsByClassName("image");
-    var img = null;
+    var classname = document.getElementsByClassName("img");
 
-    // Could be realized with a for of loop (new js-spec)
-    // Add event-listeners iterative to all elements with class "image"
-    for (var i = 0; i < images.length; i++) {
-        img = images[i];
-        img.addEventListener('mouseover',
-            function () {
-                showImageButtons(img);
-            }
-        );
-        img.addEventListener('mouseout',
-            function () {
-                hideImageButtons(img);
-            }
-        );
-        img.addEventListener("touchstart",
-            function () {
-                showImageButtons(img);
-            }
-        );
-        img.addEventListener('touchend',
-            function () {
-                hideImageButtons(img);
-            }
-        );
+
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('mouseover', function(event) {
+            var target = event.target;
+            var btn1 = target.nextElementSibling.firstElementChild;
+            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            btn1.style.visibility = "visible";
+            btn1.style.opacity = 1;
+            btn2.style.visibility = "visible";
+            btn2.style.opacity = 1;
+
+        });
+        classname[i].addEventListener('mouseout', function(event) {
+            var target = event.target;
+            var btn1 = target.nextElementSibling.firstElementChild;
+            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            btn1.style.visibility = "hidden";
+            btn1.style.opacity = 0;
+            btn2.style.opacity = 0;
+            btn2.style.visibility = "hidden";
+        });
+        classname[i].addEventListener('touchstart', function(event) {
+            var target = event.target;
+            var btn1 = target.nextElementSibling.firstElementChild;
+            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            btn1.style.visibility = "visible";
+            btn2.style.visibility = "visible";
+            btn1.style.opacity = 1;
+            btn2.style.opacity = 1;
+        });
+        classname[i].addEventListener('touchend', function(event) {
+            var target = event.target;
+            var btn1 = target.nextElementSibling.firstElementChild;
+            var btn2 = target.nextElementSibling.firstElementChild.nextElementSibling;
+            btn1.style.visibility = "hidden";
+            btn1.style.opacity = 0;
+            btn2.style.visibility = "hidden";
+            btn2.style.opacity = 0;
+
+        });
     }
 
-    /**
-     * Show the buttons on image by changing the CSS-class
-     * @param node image dom-elment
-     */
-    function showImageButtons(node) {
-        node.innerHTML = "test";
-    }
 
-    /**
-     * Hide the buttons on image by changing the CSS-class
-     * @param node image dom-element
-     */
-    function hideImageButtons(node) {
-
-    }
 
 
 };

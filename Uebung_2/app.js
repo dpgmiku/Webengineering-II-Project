c350f0951e89f33b4e1e3c8b4634842bed5b320a
Paul Grundmann <paul.grundmann@snafu.de>
2017-04-25 13:35:49 +0200

    Aufgabe 4 complete

--- Uebung_2/app.js
+++ Uebung_2/app.js
@@ -20,8 +20,15 @@ app.get('/', function (req, res) {
  */
 app.use('/staticfiles', express.static(path.join(__dirname, '/public')));
 
-
-
+/**
+ * Aufgabe 4
+ */
+app.get('/time', function(req, res) {
+    res.set('content-type', 'text/plain');
+    var date = new Date();
+    var dateString = date.toString();
+    res.send(dateString);
+})
 
 
 

0f4191f6f9761b178e4175a545912aca8888602d
Paul Grundmann <paul.grundmann@snafu.de>
2017-04-25 13:28:12 +0200

    added gitignore for node_modules, Aufgabe 1-3 complete

--- Uebung_2/app.js
+++ Uebung_2/app.js
@@ -1,3 +1,30 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
 /**
- * Created by sunku on 24.04.2017.
+ * Aufgabe 1
  */
+app.get('/', function (req, res) {
+    res.send('<!DOCTYPE html>' +
+        '<html lang="en">' +
+        '<head><meta charset="utf-8"></head>' +
+        '<body><h1>Hello World!</h1></body>' +
+        '</html>'
+    );
+});
+
+/**
+ * Aufgabe 3
+ */
+app.use('/staticfiles', express.static(path.join(__dirname, '/public')));
+
+
+
+
+
+
+app.listen(3000, function () {
+
+});

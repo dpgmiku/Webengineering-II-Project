6c686d562398fa325d52ad455ac9f33b2fcac952
Rene <s64359@beuth-hochschule.de>
2017-05-02 22:05:11 +0200

    verbessert in UE

--- Uebung_2/app.js
+++ Uebung_2/app.js
@@ -4,17 +4,7 @@ var fs = require('fs');
 var app = express();
 
 
-/**
- * Aufgabe 1
- */
-app.get('/', function (req, res) {
-    res.send('<!DOCTYPE html>' +
-        '<html lang="en">' +
-        '<head><meta charset="utf-8"></head>' +
-        '<body><h1>Hello World!</h1></body>' +
-        '</html>'
-    );
-});
+
 
 /**
  * Aufgabe 3
@@ -25,28 +15,48 @@ app.use('/staticfiles', express.static(path.join(__dirname, '/public')));
  * Aufgabe 4
  */
 app.get('/time', function (req, res) {
-    res.set('content-type', 'text/plain');
+    res.setHeader('content-type', 'text/plain');
     var date = new Date();
     var dateString = date.toString();
     res.send(dateString);
 });
 
+
 /**
  * Aufgabe 5a
  */
 app.get('/text.txt', function (req, res) {
+	res.setHeader('content-type', 'text/plain');
     var startTimestamp = process.hrtime();
+
     fs.readFile(path.join(__dirname, '/textfile.txt'), 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
         var endtime = process.hrtime();
         var total = [endtime[0] - startTimestamp[0], endtime[1] - startTimestamp[1]];
+
         var time = total[0] + 's : ' + total[1] + 'ns';
-        res.set('content-type', 'text/plain');
-        res.send(data + time);
+        console.log(data);
+
+	res.send(data + time);
+
     });
+})
+
+
+/**
+ * Aufgabe 1
+ */
+app.use('/', function (req, res) {
+    res.send('<!DOCTYPE html>' +
+        '<html lang="en">' +
+        '<head><meta charset="utf-8"></head>' +
+        '<body><h1>Hello World!</h1></body>' +
+        '</html>'
+    );
 });
+
 
 app.listen(3000, function () {
 

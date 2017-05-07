138f124b657aac87c9590382b57ac4e09a02f260
Chun Kit Kevin Ma <machunki.km@googlemail.com>
2017-05-07 20:30:56 +0200

    final or almost final, added the last bonus task

--- Uebung_2/app.js
+++ Uebung_2/app.js
@@ -29,13 +29,36 @@ app.get('/time', function (req, res) {
  *@returns error, if such occurs, as log in the console.
  */
 app.get('/text.txt', function (req, res) {
-	res.setHeader('content-type', 'text/plain');
     var startTimestamp = process.hrtime();
+    res.setHeader('content-type', 'text/plain');
 
     fs.readFile(path.join(__dirname, '/textfile.txt'), 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
+        var endtime = process.hrtime();
+        var total = [endtime[0] - startTimestamp[0], endtime[1] - startTimestamp[1]];
+
+        var time = total[0] + 's : ' + total[1] + 'ns';
+
+	res.send(data + time);
+
+    });
+})
+
+/**
+ * Task 5b
+ * memoizing the readfile function in the cache, so it would be much faster as regular. 
+ * Limitations: consuming additional memory and output must depending only on their inputs. 
+ * @param func
+ * @returns {Function}
+ */
+
+app.get('/text2.txt', function (req, res) {
+    var startTimestamp = process.hrtime();
+    res.setHeader('content-type', 'text/plain');
+
+    reader(path.join(__dirname, '/textfile.txt'), function (data) {
         var endtime = process.hrtime();
         var total = [endtime[0] - startTimestamp[0], endtime[1] - startTimestamp[1]];
 
@@ -47,11 +70,46 @@ app.get('/text.txt', function (req, res) {
     });
 })
 
-/**
- * Task 5b
- *
- * Limitations: consuming additional memory and output must depending only on their inputs. 
- */
+
+
+function memoize(func){
+//stores the data into the cache
+var cache = {};
+    
+//please read the data from the cache if it was already stored there, if it's not this case - read the file and store it in the cache
+return function(path, callback){
+if (cache[path] === undefined){
+    func(path, function(data){
+         if (data){
+        
+        cache[path]=data;
+    }
+   callback(cache[path]);
+         });
+}
+    else {
+        callback(cache[path]);
+        
+    }
+};
+};
+ 
+//writes the result of memoized function into the variable reader
+
+var reader = memoize(function(path, callback){
+fs.readFile(path, 'utf8', function(err,data){
+    if (err){
+   
+    return console.log(err);
+    }
+    callback(data);
+    
+});
+});
+                     
+                     
+                     
+    
 
 
 

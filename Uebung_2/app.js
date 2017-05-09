ea2c07f0153e7fff284827ac4cb13c04a745464b
Paul Grundmann <paul.grundmann@snafu.de>
2017-05-09 10:55:06 +0200

    added Uebung 3, added Doc for function in uebung2

--- Uebung_2/app.js
+++ Uebung_2/app.js
@@ -1,3 +1,4 @@
+"use strict";
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -40,20 +41,17 @@ app.get('/text.txt', function (req, res) {
         var total = [endtime[0] - startTimestamp[0], endtime[1] - startTimestamp[1]];
 
         var time = total[0] + 's : ' + total[1] + 'ns';
-
-	res.send(data + time);
-
+        res.send(data + time);
     });
-})
+});
 
 /**
  * Task 5b
- * memoizing the readfile function in the cache, so it would be much faster as regular. 
- * Limitations: consuming additional memory and output must depending only on their inputs. 
+ * memoizing the readfile function in the cache, so it would be much faster as regular.
+ * Limitations: consuming additional memory and output must depending only on their inputs.
  * @param func
  * @returns {Function}
  */
-
 app.get('/text2.txt', function (req, res) {
     var startTimestamp = process.hrtime();
     res.setHeader('content-type', 'text/plain');
@@ -65,72 +63,66 @@ app.get('/text2.txt', function (req, res) {
         var time = total[0] + 's : ' + total[1] + 'ns';
         console.log(data);
 
-	res.send(data + time);
+        res.send(data + time);
 
     });
-})
-
-
-
-function memoize(func){
-//stores the data into the cache
-var cache = {};
-    
-//please read the data from the cache if it was already stored there, if it's not this case - read the file and store it in the cache
-return function(path, callback){
-if (cache[path] === undefined){
-    func(path, function(data){
-         if (data){
-        
-        cache[path]=data;
-    }
-   callback(cache[path]);
-         });
-}
-    else {
-        callback(cache[path]);
-        
-    }
-};
-};
- 
-//writes the result of memoized function into the variable reader
-
-var reader = memoize(function(path, callback){
-fs.readFile(path, 'utf8', function(err,data){
-    if (err){
-   
-    return console.log(err);
-    }
-    callback(data);
-    
 });
-});
-                     
-                     
-                     
-    
-
-
-
-
-
 
 /**
- * Task 1
- * returns "Hello World!" as html by requesting all paths on the server excluding previously stated. It has to be written on the end of the file, so it would not overwrite previously methods
+ *
+ * @param func function to store in memory
+ * @returns {Function}
  */
-app.use('/', function (req, res) {
-    res.send('<!DOCTYPE html>' +
-        '<html lang="en">' +
-        '<head><meta charset="utf-8"></head>' +
-        '<body><h1>Hello World!</h1></body>' +
-        '</html>'
-    );
-});
+function memoize(func) {
+//stores the data into the cache
+    var cache = {};
+
+// please read the data from the cache if it was already stored there,
+// if it's not this case - read the file and store it in the cache
+    return function (path, callback) {
+        if (cache[path] === undefined) {
+            func(path, function (data) {
+                if (data) {
+
+                    cache[path] = data;
+                }
+                callback(cache[path]);
+            });
+        }
+        else {
+            callback(cache[path]);
+
+        }
+    };
+}
+
+//writes the result of memoized function into the variable reader
+    var reader = memoize(function (path, callback) {
+        fs.readFile(path, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            callback(data);
+        });
+    });
 
 
+    /**
+     * Task 1
+     * returns "Hello World!" as html by requesting all paths on the server
+     * excluding previously stated. It has to be written on the end of the file,
+     * so it would not overwrite previously methods
+     */
+    app.use('/', function (req, res) {
+        res.send('<!DOCTYPE html>' +
+            '<html lang="en">' +
+            '<head><meta charset="utf-8"></head>' +
+            '<body><h1>Hello World!</h1></body>' +
+            '</html>'
+        );
+    });
 
-app.listen(3000, function () {
 
-});
+    app.listen(3000, function () {
+
+    });
